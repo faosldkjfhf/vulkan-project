f0da7f174d2010f21352ff9c749f5bb299cd49c6
Kevin <kliu0932@gmail.com>
2024-06-28 15:35:08 -0400

    Debug messenger set up

--- include/vulkan_program.hpp
+++ include/vulkan_program.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
 #include "glfw.hpp"
+#include <iostream>
 #include <vector>
+
+struct QueueFamilyIndices {
+  int graphicsFamily = -1;
+};
 
 class VulkanProgram {
 public:
@@ -12,10 +17,43 @@ public:
 
 private:
   VkInstance _instance;
+  VkDebugUtilsMessengerEXT _debugMessenger;
+  VkPhysicalDevice _physicalDevice = VK_NULL_HANDLE;
   const std::vector<const char *> _validationLayers = {
       "VK_LAYER_KHRONOS_validation",
   };
 
   void createInstance();
+  void setupDebugMessenger();
+  void pickPhysicalDevice();
+
+  bool isDeviceSuitable(VkPhysicalDevice device);
+
+  const std::vector<const char *> getRequiredExtensions();
   const bool checkValidationLayerSupport();
+
+  // TODO: Move to a separate utils class
+  void populateDebugMessengerCreateInfo(
+      VkDebugUtilsMessengerCreateInfoEXT &createInfo);
+  VkResult createDebugUtilsMessengerEXT(
+      VkInstance instance,
+      const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
+      const VkAllocationCallbacks *pAllocator,
+      VkDebugUtilsMessengerEXT *pDebugMessenger);
+  void destroyDebugUtilsMessengerEXT(VkInstance instance,
+                                     VkDebugUtilsMessengerEXT debugMessenger,
+                                     const VkAllocationCallbacks *pAllocator);
+
+  static VKAPI_ATTR VkBool32 VKAPI_CALL
+  debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                VkDebugUtilsMessageTypeFlagsEXT messageType,
+                const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+                void *pUserData) {
+    if (messageSeverity > VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+      std::cout << messageSeverity << ": " << pCallbackData->pMessage
+                << std::endl;
+    }
+
+    return VK_FALSE;
+  }
 };
--- src/vulkan_program.cpp
+++ src/vulkan_program.cpp
@@ -3,9 +3,40 @@
 #include <cstring>
 #include <iostream>
 
-VulkanProgram::~VulkanProgram() { vkDestroyInstance(_instance, nullptr); }
+VulkanProgram::~VulkanProgram() {
+  destroyDebugUtilsMessengerEXT(_instance, _debugMessenger, nullptr);
+  vkDestroyInstance(_instance, nullptr);
+}
 
-void VulkanProgram::initVulkan() { createInstance(); }
+void VulkanProgram::initVulkan() {
+  createInstance();
+  setupDebugMessenger();
+  pickPhysicalDevice();
+}
+
+void VulkanProgram::populateDebugMessengerCreateInfo(
+    VkDebugUtilsMessengerCreateInfoEXT &createInfo) {
+  createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+  createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+                               VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                               VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+  createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                           VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                           VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+  createInfo.pfnUserCallback = debugCallback;
+  createInfo.pUserData = nullptr;
+}
+
+void VulkanProgram::setupDebugMessenger() {
+  VkDebugUtilsMessengerCreateInfoEXT createInfo = {};
+  populateDebugMessengerCreateInfo(createInfo);
+
+  if (createDebugUtilsMessengerEXT(_instance, &createInfo, nullptr,
+                                   &_debugMessenger) != VK_SUCCESS) {
+    throw std::runtime_error("Couldn't create debug messenger");
+  }
+}
 
 void VulkanProgram::createInstance() {
   if (!checkValidationLayerSupport()) {
@@ -24,18 +55,68 @@ void VulkanProgram::createInstance() {
   createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
   createInfo.pApplicationInfo = &appInfo;
 
+  std::vector<const char *> extensions = getRequiredExtensions();
+
+  VkDebugUtilsMessengerCreateInfoEXT debugInfo = {};
+  populateDebugMessengerCreateInfo(debugInfo);
+
+  createInfo.enabledExtensionCount = extensions.size();
+  createInfo.ppEnabledExtensionNames = extensions.data();
+  createInfo.enabledLayerCount = _validationLayers.size();
+  createInfo.ppEnabledLayerNames = _validationLayers.data();
+  createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT *)&debugInfo;
+
+  if (vkCreateInstance(&createInfo, nullptr, &_instance) != VK_SUCCESS) {
+    throw std::runtime_error("Failed to create Vulkan instance");
+  }
+}
+
+void VulkanProgram::pickPhysicalDevice() {
+  unsigned int deviceCount = 0;
+  vkEnumeratePhysicalDevices(_instance, &deviceCount, nullptr);
+
+  if (deviceCount == 0) {
+    throw std::runtime_error("Failed to find GPUs with Vulkan support");
+  }
+
+  std::vector<VkPhysicalDevice> devices(deviceCount);
+  vkEnumeratePhysicalDevices(_instance, &deviceCount, devices.data());
+
+  for (const auto &device : devices) {
+    if (isDeviceSuitable(device)) {
+      _physicalDevice = device;
+      break;
+    }
+  }
+
+  if (_physicalDevice == VK_NULL_HANDLE) {
+    throw std::runtime_error("Failed to find a suitable GPU");
+  }
+}
+
+bool VulkanProgram::isDeviceSuitable(VkPhysicalDevice device) {
+  VkPhysicalDeviceProperties deviceProperties = {};
+  vkGetPhysicalDeviceProperties(device, &deviceProperties);
+
+  VkPhysicalDeviceFeatures deviceFeatures;
+  vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+  // TODO: Ensure certain features/properties, right now doesn't matter
+
+  return true;
+}
+
+const std::vector<const char *> VulkanProgram::getRequiredExtensions() {
   unsigned int glfwExtensionCount = 0;
   const char **glfwExtensions;
 
   glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-  createInfo.enabledExtensionCount = glfwExtensionCount;
-  createInfo.ppEnabledExtensionNames = glfwExtensions;
-  createInfo.enabledLayerCount = 0;
+  std::vector<const char *> extensions(glfwExtensions,
+                                       glfwExtensions + glfwExtensionCount);
+  extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 
-  if (vkCreateInstance(&createInfo, nullptr, &_instance) != VK_SUCCESS) {
-    throw std::runtime_error("Failed to create Vulkan instance");
-  }
+  return extensions;
 }
 
 const bool VulkanProgram::checkValidationLayerSupport() {
@@ -61,4 +142,27 @@ const bool VulkanProgram::checkValidationLayerSupport() {
   }
 
   return true;
+}
+
+VkResult VulkanProgram::createDebugUtilsMessengerEXT(
+    VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
+    const VkAllocationCallbacks *pAllocator,
+    VkDebugUtilsMessengerEXT *pDebugMessenger) {
+  auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+      instance, "vkCreateDebugUtilsMessengerEXT");
+  if (func != nullptr) {
+    return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+  } else {
+    return VK_ERROR_EXTENSION_NOT_PRESENT;
+  }
+}
+
+void VulkanProgram::destroyDebugUtilsMessengerEXT(
+    VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger,
+    const VkAllocationCallbacks *pAllocator) {
+  auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+      instance, "vkDestroyDebugUtilsMessengerEXT");
+  if (func != nullptr) {
+    func(instance, debugMessenger, pAllocator);
+  }
 }
